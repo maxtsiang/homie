8e3789daac10be421a4bdb4a12b166c34e6fb461
Max Tsiang <mtsiang@Maxs-MacBook-Pro.local>
2021-01-18 17:37:56 +0800

    update chat contact ui

--- src/components/ChatContacts.js
+++ src/components/ChatContacts.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import Divider from "@material-ui/core/Divider";
+import Box from "@material-ui/core/Box";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
@@ -17,31 +18,46 @@ const useStyles = makeStyles((theme) => ({
   inline: {
     display: "inline",
   },
+  sender: {
+    display: "flex",
+    alignItems: "center",
+  },
+  listItem: {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
 }));
-
-const ChatPreview = (userName, avatar, lastmsg) => {
-  return (
-    <ListItem alignItems="flex-start">
-      <ListItemAvatar>
-        <Avatar alt="Max Tsiang" src={avatar} />
-      </ListItemAvatar>
-      <ListItemText
-        primary={userName}
-        secondary={<React.Fragment>{lastmsg}</React.Fragment>}
-      />
-    </ListItem>
-  );
-};
 
 const ChatContacts = () => {
   const classes = useStyles();
+
+  const ChatPreview = (userName, avatar, lastmsg) => {
+    return (
+      <ListItem className={classes.listItem}>
+        <Box className={classes.sender}>
+          <ListItemAvatar>
+            <Avatar alt="Max Tsiang" src={avatar} />
+          </ListItemAvatar>
+          <ListItemText
+            primary={userName}
+            secondary={<React.Fragment>{lastmsg}</React.Fragment>}
+          />
+        </Box>
+        <Box>
+          <Typography variant="subtitle2">1 MIN AGO</Typography>
+        </Box>
+      </ListItem>
+    );
+  };
+
   return (
     // obviously this will have to be a template that pulls data from the database
     <List>
       {ChatPreview("Max Tsiang", null, "hello this is a preview")}
-      <Divider variant="inset" component="li" />
+
       {ChatPreview("Max Tsiang", null, "hello this is another preview")}
-      <Divider variant="inset" component="li" />
+
       {ChatPreview("Max Tsiang", null, "wfjbfjewhflef")}
     </List>
   );
--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,14 +1,29 @@
-import { Grid } from "@material-ui/core";
+import { Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 
 import ChatWindow from "../components/ChatWindow";
 import ChatContacts from "../components/ChatContacts";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles({
+  container: {
+    margin: "3em",
+  },
+  header: {
+    marginTop: "1em",
+    marginLeft: "0.5em",
+  },
+});
 
 const Chat = () => {
+  const classes = useStyles();
   return (
     <div>
-      <Grid container style={{ marginTop: "5em" }}>
+      <Grid container className={classes.container}>
         <Grid item xs={3}>
+          <Typography variant="h3" className={classes.header}>
+            Chats{" "}
+          </Typography>
           <ChatContacts></ChatContacts>
         </Grid>
         <Grid item xs={9}>
